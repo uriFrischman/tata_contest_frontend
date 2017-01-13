68dfbe54527356eec79c736ce4140361ff466626
Uri <uri.frischman@gmail.com>
2017-01-13 14:10:36 -0500

    added speration of search results

--- app.js
+++ app.js
@@ -23,8 +23,8 @@ var app = angular.module('tataSite', []);
 app.controller('tataCtrl', function ($scope, $http) {
     $scope.searchOption = {};
     $scope.loading = false;
+    $scope.links = {};
     $scope.search = function () {
-        console.log($scope.searchQuery);
         $scope.loading = true;
         var sitesToSearch = Object.keys($scope.searchOption);
         $scope.results = [];
@@ -32,11 +32,11 @@ app.controller('tataCtrl', function ($scope, $http) {
             $scope.site = site;
             $http.get('https://tata-contest.herokuapp.com/' + site +'/' + $scope.searchQuery).then(function (data) {
                 console.log(data.data);
-                $scope.results.push(data.data)
+                $scope.results.push(data.data);
+                $scope.links[site] = data.data[site];
             })
-
-
-        })
+        });
+        console.log($scope.results);
 
         console.log(Object.keys($scope.searchOption));
         $scope.loading = false;

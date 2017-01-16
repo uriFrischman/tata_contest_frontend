15078cf613bc9198bf3e9f0c5ea25987d833b4b4
Uri <uri.frischman@gmail.com>
2017-01-16 01:07:00 -0500

    complete contest submissoin

--- app.js
+++ app.js
@@ -1,36 +1,7 @@
-/**
- * Created by Frischman on 2017-01-12.
- */
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 var app = angular.module('tataSite', []);
 
 app.controller('tataCtrl', function ($scope, $http) {
-    $scope.searchOption = {
-        'wikipedia': null,
-        'google': null,
-        'youtube': null,
-        'new_york_times': null,
-        'summary': null,
-        'reference': null,
-        'patents': null,
-        'cases': null
-    };
+    $scope.searchOption = {};
     $scope.links = {};
     $scope.search = function () {
         var sitesToSearch = Object.keys($scope.searchOption);
@@ -43,22 +14,7 @@ app.controller('tataCtrl', function ($scope, $http) {
                 $scope.links[site] = data.data[site];
             })
         });
+        $scope.searchOption = {};
     };
-
-    $scope.selectAll = function () {
-        if ($scope.searchOption.selectAll) {
-            angular.forEach(Object.keys($scope.searchOption), function (site) {
-                $scope.searchOption[site] = true;
-            } )
-        }
-        else {
-            angular.forEach(Object.keys($scope.searchOption), function (site) {
-                $scope.searchOption[site] = false;
-            } )
-        }
-    }
-
-
-
 
 })
